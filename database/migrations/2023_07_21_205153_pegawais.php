bb12a9fe35779083c60590ead53314a14de0845c
IzzatulYd <izzatulyazidah131@gmail.com>
2023-07-22 04:25:51 +0700

    first commit

--- database/migrations/2023_07_21_205153_pegawais.php
+++ database/migrations/2023_07_21_205153_pegawais.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateProjectsTable extends Migration
+class Pegawais extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,12 @@ class CreateProjectsTable extends Migration
      */
     public function up()
     {
-        Schema::create('projects', function (Blueprint $table) {
+        Schema::create('pegawais', function (Blueprint $table) {
             $table->id();
-            $table->string('title');
-            $table->string('description');
+            $table->String('nama_pegawai','255');
+            $table->String('jabatan','255');
             $table->timestamps();
+            
         });
     }
 
@@ -28,6 +29,6 @@ class CreateProjectsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('projects');
+        //
     }
 }
--- database/migrations/2023_07_21_205939_jabatan_pegawais.php
+++ database/migrations/2023_07_21_205939_jabatan_pegawais.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateKontraksTable extends Migration
+class JabatanPegawais extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,9 @@ class CreateKontraksTable extends Migration
      */
     public function up()
     {
-        Schema::create('kontraks', function (Blueprint $table) {
+        Schema::create('jabatan_pegawais', function (Blueprint $table) {
             $table->id();
-            $table->char('jabatan_pegawai_id','11');
-            $table->char('pegawai_id','11');
-            $table->string('lama_kontrak','11');
+            $table->String('nama_jabatan','255');
             $table->timestamps();
             
         });
@@ -30,6 +28,6 @@ class CreateKontraksTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('kontraks');
+        //
     }
 }
